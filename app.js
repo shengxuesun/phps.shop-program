f174b378fdc9fb6de01773418c12cc778b262611
wyc <wyc0859@qq.com>
2018-09-21 09:30:45 +0800

    1.0

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 import { Token } from 'utils/token.js';
 var token = new Token(); 
 
-App({
-
+App({ 
   onLaunch: function () {    
     token.verify(); //小程序初始化完成时就,调用token文件的verify方法，向api验证token
   }
